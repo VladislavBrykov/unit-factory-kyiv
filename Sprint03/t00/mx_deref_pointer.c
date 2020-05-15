f74da4cfd1de65971b7e5a32fd06429a69fb496f
Vladislav Brykov <vbrykov@e2r6p4.unit.ua>
2020-05-15 23:27:49 +0300

    t00 main

--- Sprint03/t00/mx_deref_pointer.c
+++ Sprint03/t00/mx_deref_pointer.c
@@ -1,3 +1,19 @@
-void mx_deref_pointer(char ******str) {		//new function
-	*****str = "Follow the white rabbit!";  //zapusk pointerov cherez main 
+#include <stdio.h>
+
+void mx_deref_pointer(char ******str)
+{
+	*****str = "Hello";
+}
+
+int main()
+{
+	char *str;
+	char **str1 = &str;
+	char ***str2 = &str1;
+	char ****str3 = &str2;
+	char *****str4 = &str3;
+	char ******str5 = &str4;
+	mx_deref_pointer(str5);
+	printf("%s\n", str);
+	return 0;
 }
