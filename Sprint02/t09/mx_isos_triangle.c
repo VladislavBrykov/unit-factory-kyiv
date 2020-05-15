62cb4ceb01645f6f9b0176ecd689575db74446dc
Vladislav Brykov <vbrykov@e2r6p4.unit.ua>
2020-05-15 20:19:30 +0300

    t09.t10

--- Sprint02/t09/mx_isos_triangle.c
+++ Sprint02/t09/mx_isos_triangle.c
@@ -1,18 +1,21 @@
-//#include <stdio.h>
+#include <unistd.h>
 
-void mx_printchar(char c);
+void mx_printchar(char c)
+{
+	write(1, &c, 1);
+}
 
 void mx_isos_triangle(unsigned int length, char c)
 {
-	for(unsigned int i = 0; i<length; i++) {	//1 cikl - stolbci
-		for(unsigned int j = 0; j <= i; j++)  //2 cikl ctroki
-		mx_printchar(c);
+	for(unsigned int i=0; i<=length; i++) {
+		for(unsigned int j=0; j<=i; j++)
+			mx_printchar(c);
 		mx_printchar('\n');
 	}
 }
 
-//int main()
-//{
-//	mx_triangle(3, '*');
-//	return 0;
-//}
+int main()
+{
+	mx_isos_triangle(4, '#');
+	return 0; 
+}
--- Sprint02/t10/mx_multiple_number.c
+++ Sprint02/t10/mx_multiple_number.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
 #include <stdbool.h>
 
-bool mx_multiple_number(int n, int mult) {
-	if (n * n == mult)			
-		printf("%d", 2);			
-	else
-		printf("%d", 1);
-	return 0;
+bool mx_multiple_number(int n, int mult)
+{
+	return(mult % n == 0);
 }
 
 int main()
 {
-	mx_multiple_number(0, 1);
+	mx_multiple_number(3, 9);
+	return 0;
 }
-
