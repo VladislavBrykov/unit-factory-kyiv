7b65704dc94de49d5facb5017a4523daa03729ec
Vladislav Brykov <vbrykov@e2r7p9.unit.ua>
2020-06-01 11:10:24 +0300

    mx_print_name.c

--- Sprint05/t00/mx_print_name.c
+++ Sprint05/t00/mx_print_name.c
@@ -1,19 +1,35 @@
+#include <stdio.h>
 #include <unistd.h>
-  
-void mx_printchar(char c);
-int mx_strlen(const char *s);
-void mx_printstr(const char*s);
-void mx_printint(int n);
 
-int main (int argc, char *argv[]) {
-    mx_printstr(argv[0]);
-    mx_printchar('\n');
-    mx_printint(argc);
-    mx_printchar('\n');
+void mx_printchar(char c) {
+	write(1, &c, 1);
 }
 
-//dla zapuska kompilim i vvodim
-//./mx_print_name Follow the white rabbit | cat -e
-//vidim argv[0] i args
-//./mx_print_name$
-//5$
+void mx_printint(int i) {
+	if(i < 0)
+		mx_printchar('-');
+	unsigned int num = i;
+	if(num > 9)
+		mx_printint(num / 10);
+	mx_printchar(num % 10 + 48);
+}
+
+int mx_strlen(const char *c) {
+	int num = 0;
+	while(c[num])
+		num++;
+	return num;
+}
+
+void mx_printstr(const char *s) {
+	write(1, s, mx_strlen(s));
+}
+
+int main(int argc, char * argv[]) {
+	mx_printstr(argv[0]);
+	mx_printchar('\n');
+	mx_printint(argc);
+	mx_printchar('\n');
+}
+
+
