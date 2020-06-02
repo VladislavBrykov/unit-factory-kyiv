a2347bd21a37addc6f2e150c940de18df67752cb
Vladislav Brykov <vbrykov@e1r5p10.unit.ua>
2020-06-02 15:30:43 +0300

    mx_factorial_rec.c

--- Sprint05/t07/mx_factorial_rec.c
+++ Sprint05/t07/mx_factorial_rec.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int mx_factorial_rec(int n) {
-	return n<=1 ? 1 : n * mx_factorial_rec(n-1);
+	if((n > 1) && (n < 19))
+		return (n * mx_factorial_rec(n - 1));
+	if((n < 0) || (n > 19))
+		return 0;
+	if(n == 0)
+		return 1;
+    	return n;
 }
 
-//int main ()
-//{
-//  int n;
-//  while (scanf ("%d", &n) == 1) {
-//    printf ("%d\n", mx_factorial_rec(n));
-//  }
-//  return 0;
-//}
+int main ()
+{
+printf ("%d\n", mx_factorial_rec(5));
+return 0;
+}
 
