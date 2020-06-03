ba899c7f6ba9e0b260ff20edc1685f5bcd618da7
Vladislav Brykov <vbrykov@e1r5p10.unit.ua>
2020-06-03 18:36:57 +0300

    mx_binary_search.c

--- Sprint06/t03/mx_binary_search.c
+++ Sprint06/t03/mx_binary_search.c
@@ -1,25 +1,43 @@
-int mx_strcmp( const char *s1, const char *s2);
+#include <stdio.h>
+#include <unistd.h>
 
-int mx_binary_search(char **arr, int size, const char *s, int *count){
-  int firstEl = 0;  //obiavlenie peremennih
-  int lastEl = size - 1; //obiavlenie peremennih
-  int middleEl; //obiavlenie peremennih
-
-  if(!*arr) {  // yslovie esli
-    return -1; //togda
-  } else { //inache
-    while (firstEl <= lastEl) {  //zapuskaem cikl while
-      (*count)++;
-      middleEl = (firstEl + lastEl) / 2;
-      if (mx_strcmp(arr[middleEl], s) > 0) {
-	lastEl = middleEl - 1;
-      }else if (mx_strcmp(arr[middleEl], s) == 0) { //inache esli
-	return middleEl;  //rezultat
-      } else {
-	firstEl = middleEl + 1;
-      }
-    }
-    *count = 0;
-    return -1;
-  }  
+int mx_strcmp(const char *s1, const char *s2) {
+    int i = 0;
+    while(s1[i]) {
+        if(s1[i] != s2[i])
+            return(s1[i] - s2[i]);
+        if(s1[i] == s2[i])
+            i++;
+        }
+    return 0;
 }
+
+int mx_binary_search(char **arr, int size, const char *s, int *count) {
+	int first = 0;
+	int last = size - 1;
+	int middle;
+	while(first <= last) {
+		middle = (first + last) / 2;
+			if(mx_strcmp(arr[middle], s) > 0) {
+				last = middle - 1;
+				(*count)++;
+			}
+			else if(mx_strcmp(arr[middle], s) < 0) { 
+				(*count)++;
+				first = middle + 1;
+			}
+			else {
+				(*count)++;
+				return middle;
+			}	
+	}
+	count = 0;
+	return -1;
+}
+
+int main() {
+char *arr[6] = {"222", "Abcd", "aBc", "ab", "az", "z"};
+int count = 0;
+printf("%d\n", mx_binary_search(arr, 6,"ab", &count));
+printf("%d\n", count);
+}	
