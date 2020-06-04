6ae1657e79d0faaf2ba872eb736e73205aa1c764
Vladislav Brykov <vbrykov@e1r5p10.unit.ua>
2020-06-04 17:22:46 +0300

    mx_strnew.c

--- Sprint07/t00/mx_strnew.c
+++ Sprint07/t00/mx_strnew.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 char *mx_strnew(const char size) {
-	char *arr = malloc(size + 1);
+	char *arr = malloc(size * sizeof(*arr) + 1);
 
 	if(arr != NULL && size > 0) {
 		for(int i = 0; i <= size+1; i++) 
@@ -20,5 +20,8 @@ int main()
 
 /*Выделили память под указатель на массив arr. Если память выделена arr != NULL
 заполняем выделеную память. 
+
+malloc(size * sizeof(*arr) + 1) - размер умножаем тип char 1bt, int уже 4 будет
+
 size + 1 - указана в задании, выделить память size и добавить еще один символ онца строки.
 Если память не выделена malloc*/
