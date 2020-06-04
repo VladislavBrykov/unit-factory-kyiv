7ed538056f8cc8448f5d27e8b314d0482fa81d18
Vladislav Brykov <vbrykov@e1r5p10.unit.ua>
2020-06-04 15:21:41 +0300

    mx_strjoin.c

--- Sprint07/t02/mx_strcpy.c
+++
@@ -1,9 +0,0 @@
-char *mx_strcpy(char *dst, const char *src) {
-    for (int i = 0; ; i++) {
-        dst[i] = src[i];
-        if (src[i] == '\0') {
-            break;
-        }
-    }
-    return dst;
-}
