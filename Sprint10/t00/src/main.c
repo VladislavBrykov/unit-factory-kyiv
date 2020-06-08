08220071b53318c455e9de59d825726d0ae26d11
Vladislav Brykov <vbrykov@e1r5p8.unit.ua>
2020-06-08 15:43:16 +0300

    read_file

--- Sprint10/t00/src/main.c
+++ Sprint10/t00/src/main.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
         char buff;
         int file = 0;
-        if (argc != 2) {
+                if (argc != 2) {
                 mx_printerr("usage: ./read_file [file_path]\n");
                 return 0;
         }
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
                         return 0;
                 }
                 else {
-                while(read(file, &buff, 1))
+               while(read(file, &buff, 1))
                         write(1, &buff, 1);
                 }
                 close(file);
@@ -22,3 +22,27 @@ int main(int argc, char *argv[]) {
         return 0;
 }
 
+/*while(read(file, &buff, 1))
+ write(1, &buff, 1);
+функция рид возвращает 1 в случае успешного считывания 1 байта, 
+передает во врайт. 
+Цикл повторяется автоматически, пока пока +1 байт будет 
+считывать информацию с файла
+
+DESCRIPTION
+Create a program that prints:
+• the contents of a file given as an argument to the standard output
+• error to the stderr followed by a newline in case of any errors
+• usage: ./read_file [file_path] to the stderr followed by a newline if no or too
+many arguments have been given
+CONSOLE OUTPUT
+>./read_file | cat -e
+usage: ./read_file [file_path]
+>./read_file file.txt
+#the contents of the file
+>./read_file unknown_file | cat -e
+error
+>
+FOLLOW THE WHITE RABBIT
+man stderr
+*/
